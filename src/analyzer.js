8248f372eaffacfbfe3fa272002443e98a6cdb12
uzy3090 <lucia1688@hotmail.com>
2024-01-19 12:35:43 -0600

    update

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,37 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const wordCount = text.split(' ').length
+    //console.log(wordCount)
+    return wordCount
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const characterCount = text.split("").length
+    //console.log(characterCount)
+    return characterCount
   },
-  getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
+  // getCharacterCountExcludingSpaces: (text) => {
+  //   //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+  // },
+  // getAverageWordLength: (text) => {
+  //   //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  // },
+  // getNumberCount: (text) => {
+  //   //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  // },
+  // getNumberSum: (text) => {
+  //   //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  // },
 };
 
 export default analyzer;
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer` 
+//traerme el elemento recuento de palabras para despues poder imprimir el resultado
+
+let textarea = document.getElementsByName("user-input")[0]
+// console.log(textarea);
+textarea.addEventListener("keyup", function() { // funcion anonima
+  let evenInput= textarea.value;
+  // console.log(evenInput);
+  let resultGetWordCount=analyzer.getWordCount(evenInput)
+  // console.log(resultGetWordCount);
+  document.querySelector('li[data-testid="word-count"]').innerHTML = 'Recuento de palabras ' + resultGetWordCount
+})
+// textarea.addEventListener("vista",function() {
+//   const words= analyzer.getWordCount (textarea.value);
+//   document.querySelector(".wordCount").innerHTML= palabras + words 
+// })
+
+// 1.1 Darle evento = (accion en el navegador, como un click o mover el mouse)
+// 1.1 Continuacion: al momento que la usuaria esta escribiendo a textarea
+//Actualizar al momento de la interacciòn de textarea con la usuaria las metricas
+
+
+// Borrar las palabras de c/u de los li, querySelector es un método de JavaScript que permite seleccionar
+// el primer elemento del documento que coincide con un grupo especificado de selectores CSS
+// const wordCount = document.querySelector( ".word-count")
+//wordCount.innerHTML= "contar palabras -1 "
+// console.log (wordCount)
+//Borrar las palabras de textarea con el boton de reset y limpiar metricas)
+
+
+// llamar al metodo de analyzer donde sume las palabras que ingreso el usuario
+
