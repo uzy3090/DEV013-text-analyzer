8da9ea7da7c53247227574a330fcb68dd76b45c3
uzy3090 <lucia1688@hotmail.com>
2024-01-19 14:12:46 -0600

    modificaciones varios

--- src/analyzer.js
+++ src/analyzer.js
@@ -11,27 +11,72 @@ const analyzer = {
     //console.log(characterCount)
     return characterCount
   },
-  // getCharacterCountExcludingSpaces: (text) => {
+  getCharacterCountExcludingSpaces: (text) => {
   //   //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  // },
+    return(text.replace(/[^a-zA-Z0-9]/g, "").length)
+  },
+  // restar espacios y signos de puntuaciòn 
+  // retornar la suma de caracteres
   // getAverageWordLength: (text) => {
-  //   //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  // },
-  // getNumberCount: (text) => {
-  //   //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-  // },
-  // getNumberSum: (text) => {
+ 
+  getNumberCount: (text) =>{
+  //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string``
+  // recibimos todo el texto
+  //  tener una variable con  el total de nuemros
+    let contador=0 
+    // separar los elementos
+    const words = text.split(' ');
+    //console.log(words)
+    // Saber cuando algo es un numero
+    for(let elemento = 0; elemento< words.length; elemento++){
+
+      if(isNaN( parseInt(words[elemento]))){
+      //  console.log('No es numero'+ words[elemento]); // true--- no convierte a número
+      }else {
+       // console.log('si hay numero' + words[elemento]); 
+        if(Number(words[elemento])){
+          contador=contador+1
+        }
+      }
+    }
+    return contador
+  },
+  getNumberSum: (text) => {
   //   //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  // },
-};
+  // omitir las letras, espacios, caracteres 
+  // realizar una suma de c/u de los numeros colocados en el textarea
+    let suma=0 
+    // separar los elementos
+    const words = text.split(' ');
+    //console.log(words) 
+    for(let elemento = 0; elemento< words.length; elemento++){
 
-export default analyzer;
+      if(isNaN( parseFloat(words[elemento]))){
+        //console.log('No es numero'+ words[elemento]); // true--- no convierte a número
+      }else{
+        //console.log('si hay numero' + words[elemento]); 
+        if(Number(words[elemento])){
+          // console.log(typeof(words[elemento]))
+          const number = words [elemento];
+          suma=suma + parseFloat(number);
+        }
+      }
+    }
+    return suma
+  },
+  getAverageWordLength: (text) => {
+  //   //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  //Para encontrar el promedio de un array, tenemos que encontrar
+    //la suma de todos los elementos presentes en un array usando un bucle
+    // y luego dividir la suma con el número de elementos presentes en el array
+    const a= analyzer.getCharacterCountExcludingSpaces(text);
+    const b= analyzer.getWordCount(text);
+    const division = a/b 
+    return division.toFixed(2);
+ 
+  
 
 
-
-
-
-
-
-
-
+  },
+}
+export default analyzer
--- src/index.js
+++ src/index.js
@@ -2,33 +2,48 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer` 
 //traerme el elemento recuento de palabras para despues poder imprimir el resultado
-
-let textarea = document.getElementsByName("user-input")[0]
+const textarea = document.getElementsByName("user-input")[0]
 // console.log(textarea);
 textarea.addEventListener("keyup", function() { // funcion anonima
-  let evenInput= textarea.value;
+  const evenInput= textarea.value;
   // console.log(evenInput);
-  let resultGetWordCount=analyzer.getWordCount(evenInput)
+  const resultGetWordCount=analyzer.getWordCount(evenInput);
   // console.log(resultGetWordCount);
   document.querySelector('li[data-testid="word-count"]').innerHTML = 'Recuento de palabras ' + resultGetWordCount
+
+  const resultcharacterCount=analyzer.getCharacterCount(evenInput);
+  //console.log(resultGetCharacterCount);
+  document.querySelector('li[data-testid="character-count"]').innerHTML = 'Recuento de caracteres' + resultcharacterCount 
+
+  const resultgetCharacterCountExcludingSpaces=analyzer.getCharacterCountExcludingSpaces (evenInput)
+  //console.log(resultgetCharacterNoSpacesCount)
+  document.querySelector('li[data-testid="character-no-spaces-count"]').innerHTML='Caracteres sin espacio y puntuacion' + resultgetCharacterCountExcludingSpaces
+
+  const resultGetNumberCount= analyzer.getNumberCount (evenInput)
+  //console.log (resultGetNumberCount)
+  document.querySelector('li[data-testid="number-count"]').innerHTML= 'Recuento de numeros' + resultGetNumberCount
+
+  const resultGetNumberSum= analyzer.getNumberSum (evenInput)
+  //console.log(resultgetNumberSum)
+  document.querySelector('li[data-testid="number-sum"]').innerHTML= 'Suma total de numeros' + resultGetNumberSum
+
+  const resultGetAverageWordLength= analyzer.getAverageWordLength (evenInput)
+  //console.log(resultgetAverageSum)
+  document.querySelector('li[data-testid="word-length-average"]').innerHTML= 'Longitud media de palabras' + resultGetAverageWordLength
+
+  //Event Listener Botón 
+  const button=document.getElementById("reset-button"); 
+  button.addEventListener("click", resetMetricas) 
+  function resetMetricas(){
+    document.querySelector("textarea").value="";
+    document.querySelector('li[data-testid="word-count"]').innerHTML ="Recuento de palabras: ";
+    document.querySelector('li[data-testid="character-count"]').innerHTML = " Recuento de caracteres: ";
+    document.querySelector('li[data-testid="character-no-spaces-count"]').innerHTML="Caracteres sin espacio y puntuacion: ";
+    document.querySelector('li[data-testid="number-count"]').innerHTML = "Recuento de numeros: ";
+    document.querySelector('li[data-testid="number-sum"]').innerHTML = "Suma total Números: ";
+    document.querySelector('li[data-testid="word-length-average"]').innerHTML = "Longitud media de palabras: ";
+  
+  
+  }
 })
-// textarea.addEventListener("vista",function() {
-//   const words= analyzer.getWordCount (textarea.value);
-//   document.querySelector(".wordCount").innerHTML= palabras + words 
-// })
-
-// 1.1 Darle evento = (accion en el navegador, como un click o mover el mouse)
-// 1.1 Continuacion: al momento que la usuaria esta escribiendo a textarea
-//Actualizar al momento de la interacciòn de textarea con la usuaria las metricas
-
-
-// Borrar las palabras de c/u de los li, querySelector es un método de JavaScript que permite seleccionar
-// el primer elemento del documento que coincide con un grupo especificado de selectores CSS
-// const wordCount = document.querySelector( ".word-count")
-//wordCount.innerHTML= "contar palabras -1 "
-// console.log (wordCount)
-//Borrar las palabras de textarea con el boton de reset y limpiar metricas)
-
-
-// llamar al metodo de analyzer donde sume las palabras que ingreso el usuario
 
